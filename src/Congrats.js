4f4b9471bfa7265b789008f96c84b83d52314107
leopoldkwok <leopoldkwok@gmail.com>
2021-03-19 22:22:51 +0000

    Code the Coongrats Component

---
+++ src/Congrats.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+/**
+ * Functional react component for congratulatory message
+ * @function
+ * @param {object} props - React props
+ * @returns {JSX.element} - Rendered component, or null if `success` prop if false.
+ */
+
+// const Congrats = (props) => {
+export default (props) => {
+  if (props.success) {
+    return (
+      <div data-test='component-congrats'>
+        <span data-test='component-message'>
+          Congratulations! You guessed the WORD!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test='component-congrats' />;
+  }
+};
