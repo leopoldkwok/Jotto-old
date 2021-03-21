e3e59723547dc333e041a515755adaa6be1d887b
leopoldkwok <leopoldkwok@gmail.com>
2021-03-21 12:56:41 +0000

    Prepare Input for Writing setState Tests

--- src/Input.js
+++ src/Input.js
@@ -2,7 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Input({ secretWord }) {
-  return <div data-test='component-input'></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  return (
+    <div data-test='component-input'>
+      <form className='form-inline'>
+        <input
+          data-test='input-box'
+          className='mb-2 mx-sm-3'
+          type='text'
+          placeholder='enter guess'
+        />
+        <button data-test='submit-button' className='btn btn-primary mb-2'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 Input.propTypes = {
